6ca8d48a38963da49afb3df9a4c69a1c515a8256
Cong.Bu <Cong.Bu@connext.com.cn>
2021-06-30 14:11:20 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# circular-container
-circular-container
+### Examples, 样例
+<img src="https://img-blog.csdnimg.cn/20210402135934396.png?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L2JvY29uZ2Jv,size_16,color_FFFFFF,t_70" width="300">
+
+### Notice
+Not support IE 不支持IE浏览器
+## Installation
+**install with NPM**
+```bash
+npm install circular-container --save
+```
+## Usage
+**In template**
+
+```html
+<div id="circular-container"></div>
+```
+
+```js
+import circularContainer from "circular-container"
+circularContainer({
+  text: "长文本",
+  fontSize: 14,
+  width: 200,
+  height: 200,
+  color: '#000',
+  background: 'deepskyblue',
+  emptyFirstLine: true
+})
+```
+Parameter | Explanation
+----|----
+text | 文本（必填）
+fontSize |  字体大小
+width | 宽度
+height | 高度
+color | 文字的颜色
+background | 背景颜色
+emptyFirstLine | 空出首行
+padding | 内边距
---
+++ index.js
@@ -0,0 +1,40 @@
+/*
+ * @Author: Cong.Bu 
+ * @Date: 2021-06-30 10:32:57 
+ * @Last Modified by: Cong.Bu
+ * @Last Modified time: 2021-06-30 11:03:42
+ */
+
+/*
+  * 参数 data
+  * fontSize 文字大小
+  * text 文本
+  * width 宽度
+  * height 高度 可选（不填与width同值）
+  * color 文字颜色
+  * background 背景颜色
+  * emptyFirstLine 空出首行
+*/
+var circularContainer = (data) => {
+  var circularDOM = document.getElementById('circular-container')
+  if (circularDOM) {
+    if (!data.height) data.width = 200
+    if (!data.height) data.height = data.width
+    if (!data.fontSize) data.fontSize = 14
+    if (!data.padding) data.padding = 5
+    if (!data.color) data.color = '#000'
+    if (!data.background) data.background = 'deepskyblue'
+    if (!data.emptyFirstLine) data.emptyFirstLine = false
+    var inputDOM = `<div class="circle-box" style="width: ${data.width}px;height: ${data.height}px;border-radius: 50%;background: ${data.background};padding: ${data.padding}px;">
+      <span class="content-before" style="float: left;width: 50%;height: 100%;shape-outside: radial-gradient(farthest-side ellipse at right, transparent 98%, red);"></span>
+      <span class="content-after" style="float: right;width: 50%;height: 100%;shape-outside: radial-gradient(farthest-side ellipse at left, transparent 98%, red);"></span>
+      <p>
+        ${data.emptyFirstLine?'<br />':''}
+        ${data.text}
+      </p>
+    </div>`
+    circularDOM.innerHTML = (inputDOM)
+  }
+}
+
+export default circularContainer
